662ef9cd99be27a0739851a5ba83102d966008a4
David Guillen Fandos <david@davidgf.net>
2013-09-08 20:53:50 +0200

    Seems that mysqldb is not thread safe... at least Ubuntu implementation!

--- python/db_interface.py
+++ python/db_interface.py
@@ -1,5 +1,6 @@
 
 import MySQLdb
+from threading import Semaphore
 
 dbuser = "wwitt"
 dbpass = "112233w"
@@ -9,19 +10,23 @@ dbname = "wwitt"
 class DBInterface():
 	def __init__(self):
 		self.db = MySQLdb.connect(dbhost,dbuser,dbpass,dbname)
+		self._lock = Semaphore(1)
 		
 	def insert(self,table,dic):
+		self._lock.acquire()
 		cursor = self.db.cursor()
 		values = [ '"'+x+'"' if type(x) is not int else str(x) for x in dic.values() ]
-		print "INSERT INTO "+table+" (" + (",".join(dic.keys())) + ") VALUES (" + (",".join(values)) + ")"
 		cursor.execute("INSERT INTO "+table+" (" + (",".join(dic.keys())) + ") VALUES (" + (",".join(values)) + ")")
 		cursor.close()
 		self.db.commit()
+		self._lock.release()
 	
 	def select(self,table,cond):
+		self._lock.acquire()
 		cursor = self.db.cursor()
 		conds =  [ x + "=" + ('"'+x+'"' if type(x) is not int else str(x)) for x in cond.keys() ]
 		cursor.execute("SELECT * FROM "+table+" WHERE " + (" AND ".join(conds)) + ") VALUES (" + (",".join(values)) + ")")
 		cursor.close()
 		self.db.commit()
+		self._lock.release()
 
