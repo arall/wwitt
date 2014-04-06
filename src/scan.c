b86055542637baf01d4e4a7e7f1d048397795769
David Guillen Fandos <david@davidgf.net>
2014-04-06 20:23:14 +0200

    Adding more onflight scans

--- src/scan.c
+++ src/scan.c
@@ -78,7 +78,7 @@ struct pseudo_hdr {
 };
 
 #define MAX_TIMEOUT 10    // Max 255!
-#define MAX_OUTSTANDING_QUERIES (1024*64)
+#define MAX_OUTSTANDING_QUERIES (1024*1024*64)
 struct port_query {
 	struct in_addr ip;
 	unsigned short port;
@@ -133,7 +133,8 @@ int main(int argc, char **argv) {
 "                                                    \n"
 "         World Wide Internet Takeover Tool          \n"
 "                    Port scanner                    \n"  );
-	
+
+	printf("Using %ull MB \n", sizeof(port_scans)/1024/1024);
 	if (argc < 6) {
 		fprintf(stderr,"Usage: %s IPstart IPend ports{max 32} speed(mbps) device\n", argv[0]);
 		exit(1);
